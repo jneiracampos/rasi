beffc4a262e6305781070c5b199a5bf138c9acc8
Juan Camilo Neira Campos <j.neira@uniandes.edu.co>
2023-09-29 21:29:19 -0500

    database

--- rasi/settings.py
+++ rasi/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'rasi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'monitoring_db',
+        'USER': 'monitoring_user',
+        'PASSWORD': 'isis2503',
+        'HOST': '10.128.0.6',
+        'PORT': '8080',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
